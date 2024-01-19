ca7917f4eeed95299e59ce4ef1eea09fadd04e84
MIn <tasabi0826@gmail.com>
2024-01-19 18:06:52 +0900

    !

---
+++ src/main/java/hello/core/order/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core.order;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)// 현재 수동으로 등록된 예제를 위해 제외함
+public class AutoAppConfig {
+
+}
